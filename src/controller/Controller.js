6cbaab7ed74261601d48910ce1ffe00e0cdc8369
kimyou1102 <yoosk5485@naver.com>
2024-11-25 13:40:54 +0900

    refactor: 휴일 비상근무 순번 잘못 입력시 평일 순번부터 재입력 받도록 수정

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -14,8 +14,7 @@ export class Controller {
     const [month, day] = await this.getAssignmentInfor();
     const dateInformation = new DateInformation(month, day);
     const monthInfo = dateInformation.getAssignedDayTypeMonthInfo();
-    const weekdays = await this.getWeekdaysInput();
-    const dayOff = await this.getDayOff();
+    const { weekdays, dayOff } = await this.getValidatedWorkerList();
     const emergencyWorkerList = new EmergencyWorkersList(weekdays, dayOff, monthInfo);
     const emergencyWorkers = emergencyWorkerList.assignEmergencyWorkerList();
     this.outputView.printWorkSchedule(month, emergencyWorkers);
@@ -31,20 +30,31 @@ export class Controller {
     return [month, day];
   }
 
+  async getValidatedWorkerList() {
+    try {
+      const weekdays = await this.getWeekdaysInput();
+      const dayOff = await this.getDayOff();
+      return { weekdays, dayOff };
+    } catch (error) {
+      this.outputView.printError(error.message);
+      return await this.getValidatedWorkerList();
+    }
+  }
+
   async getWeekdaysInput() {
-    const weekdaysInput = await this.getValidatedInputWithRetry(
+    const weekdaysInput = await this.inputView.getInput(
       '평일 비상 근무 순번대로 사원 닉네임을 입력하세요',
-      validateWorkerList,
     );
+    validateWorkerList(weekdaysInput);
 
     return weekdaysInput.split(',');
   }
 
   async getDayOff() {
-    const dayOffInput = await this.getValidatedInputWithRetry(
+    const dayOffInput = await this.inputView.getInput(
       '휴일 비상 근무 순번대로 사원 닉네임을 입력하세요',
-      validateWorkerList,
     );
+    validateWorkerList(dayOffInput);
 
     return dayOffInput.split(',');
   }
