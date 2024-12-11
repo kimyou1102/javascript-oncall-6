e21c877ef8d10fc9c5f2d2e75b3d1a1b7e76c4f8
kimyou1102 <yoosk5485@naver.com>
2024-12-11 17:24:53 +0900

    feat: 비상 근무 배정하는 기능 구현

--- src/controller/Controller.js
+++ src/controller/Controller.js
@@ -7,5 +7,48 @@ export default class Controller {
     const weekDayWorkList = await InputView.getWeekdayWorkList();
     const holidayWorkList = await InputView.getHolidayWorkList();
     const calendar = new Calendar(month, day).getCalendar();
+    this.assignmentWork(weekDayWorkList, holidayWorkList, calendar);
+  }
+
+  // eslint-disable-next-line max-lines-per-function
+  assignmentWork(weekDayWorkList, holidayWorkList, calendar) {
+    let weekDayIndex = 0;
+    let holidayIndex = 0;
+    let weekDayPrevIndex = Infinity;
+    let holidayPrevIndex = Infinity;
+
+    for (const [date, info] of Object.entries(calendar)) {
+      const before = calendar[Number(date - 1)];
+      if (info.type === '휴일') {
+        let name = holidayWorkList[holidayIndex % holidayWorkList.length];
+        // eslint-disable-next-line max-depth
+        if (holidayPrevIndex !== Infinity) {
+          name = holidayWorkList[holidayPrevIndex % holidayWorkList.length];
+          holidayPrevIndex = Infinity;
+        }
+        // eslint-disable-next-line max-depth
+        if (before && before.name === name) {
+          holidayPrevIndex = holidayIndex;
+          name = holidayWorkList[(holidayIndex + 1) % holidayWorkList.length];
+        }
+        calendar[date].name = name;
+        holidayIndex += 1;
+      }
+      if (info.type === '평일') {
+        let name = weekDayWorkList[weekDayIndex % weekDayWorkList.length];
+        // eslint-disable-next-line max-depth
+        if (weekDayPrevIndex !== Infinity) {
+          name = weekDayWorkList[weekDayPrevIndex % weekDayWorkList.length];
+          weekDayPrevIndex = Infinity;
+        }
+        // eslint-disable-next-line max-depth
+        if (before && before.name === name) {
+          weekDayPrevIndex = weekDayIndex;
+          name = weekDayWorkList[(weekDayIndex + 1) % weekDayWorkList.length];
+        }
+        calendar[date].name = name;
+        weekDayIndex += 1;
+      }
+    }
   }
 }
--- src/model/Calendar.js
+++ src/model/Calendar.js
@@ -22,6 +22,7 @@ export default class Calendar {
       calendar[i] = {
         day,
         type: this.getDayType(day, i),
+        name: '',
       };
     }
     return calendar;
