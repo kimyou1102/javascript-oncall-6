3e746ba1b8c0c6492912d21e8c4acb98faba5967
kimyou1102 <yoosk5485@naver.com>
2024-12-11 15:42:19 +0900

    feat: InputView 클래스 생성
    - 비상근무 배정일자 입력받는 메서드 생성
    - 평일 비상 근무 사원 입력받는 메서드 생성
    - 휴일 비상 근무 사원 입력받는 메서드 생성

---
+++ src/view/InputView.js
@@ -0,0 +1,26 @@
+import { Console } from '@woowacourse/mission-utils';
+
+export default class InputView {
+  static async getAllocationDate() {
+    const input = await this.getInput('비상 근무를 배정할 월과 시작 요일을 입력하세요> ');
+    return input;
+  }
+
+  static async getWeekdayWorkList() {
+    const input = await this.getInput('평일 비상 근무 순번대로 사원 닉네임을 입력하세요> ');
+    return input;
+  }
+
+  static async getHolidayWorkList() {
+    const input = await this.getInput('휴일 비상 근무 순번대로 사원 닉네임을 입력하세요> ');
+    return input;
+  }
+
+  static async getInput(message) {
+    try {
+      return await Console.readLineAsync(`${message}\n`);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
