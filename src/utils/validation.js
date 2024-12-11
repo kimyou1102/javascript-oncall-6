5d7c3d3b5dd029e36923c2d59cdf7b96c91c462a
kimyou1102 <yoosk5485@naver.com>
2024-12-11 18:06:16 +0900

    refactor: 에러 메시지 변경

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -45,12 +45,12 @@ const checkWorkerListForm = (input) => {
 
 const checkDuplication = (menus) => {
   if (menus.length !== new Set(menus).size) {
-    createError(ERROR_MESSAGE.MENU);
+    createError(ERROR_MESSAGE.INPUT);
   }
 };
 
 const checkItemEmpty = (product) => {
   if (product === '') {
-    createError('[ERROR] 올바르지 않은 형식으로 입력했습니다. 다시 입력해 주세요.');
+    createError(ERROR_MESSAGE.INPUT);
   }
 };
